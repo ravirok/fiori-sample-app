4f78122c1795fbc781fabe6c0570e6da1e2f324d
ravirok <ravirok@users.noreply.github.com>
2026-02-19 14:52:11 +0530

    Create App.controller.js

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict";
+  return Controller.extend("fiori.sample.controller.App", {
+    onPress: function () {
+      MessageToast.show("Button Pressed!");
+    }
+  });
+});
