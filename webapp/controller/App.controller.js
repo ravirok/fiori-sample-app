72d050aef00fd15fe71ca71680263abe7a301ec4
ravirok <ravirok@users.noreply.github.com>
2026-02-24 19:16:52 +0530

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-  "use strict";
-  return Controller.extend("fiori.sample.controller.App", {
-    onPress: function () {
-      MessageToast.show("Button Pressed!");
-    }
-  });
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
+    "use strict";
+ 
+    return Controller.extend("fiori.sample.controller.App", {
+        onInit: function() {
+            // App init logic
+        }
+    });
 });
