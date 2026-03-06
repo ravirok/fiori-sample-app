e4d2be2a307f938dbdc534e4e9703d8f6d00a9a8
ravirok <ravirok@users.noreply.github.com>
2026-03-06 13:01:19 +0530

    Update Component.js

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,7 +2,7 @@ sap.ui.define([
   "sap/ui/core/UIComponent"
 ], function (UIComponent) {
   "use strict";
-  return UIComponent.extend("fiori.sample.Component", {
+  return UIComponent.extend("fiori.sample.app.Component", {
     metadata: {
       manifest: "json"
     }
